97ec0544a03a048f94ae6a9eefc84741efc5f6b5
liu2005678 <60136942+liu2005678@users.noreply.github.com>
2022-06-29 10:57:37 +0800

    Update 16S.r

--- 16S.r
+++ 16S.r
@@ -10,8 +10,12 @@ base
 system("mkdir -p align")
 system("mkdir -p pile2cns")
 system("mkdir -p index")
+
+# to allow better aligning of reads that mapped to the two terminals of 16 S, 100 bp surrounding sequence (upstream 100 bp and dowstream 100 bp) were included in the 16S.fa
 system("bowtie2-build -f 16S.fa index/16s")
-# align reads to reference 16S sequence, keep aligned reads only (--no-unal)
+
+# align reads to reference 23S sequence, keep aligned reads only (--no-unal) 
+# the CPU threads used is 24 (-p 24) 
 bowtie2_cmds=paste0("bowtie2 -p 24 --no-unal -x index/16s -1 ", cleandatadir, strainsr1, " -2 ", cleandatadir, strainsr2, " -S align/",
                   base, "_16S.sam" )
 # sort the aligned reads
@@ -25,7 +29,7 @@ samtools_flow2 =function(files_base,p=24,rm=TRUE){
 }
 samtools_cmds=samtools_flow2(paste0("align/",base,"_16S"))
 
-# make pileup file 
+# make pileup file
 pileup_cmds=paste0("samtools mpileup -f 16S.fa align/",base,"_16S_sorted.bam > pile2cns/",base,"_16S.mpileup") 
 # snp and indel calling  A minimal coverage of 100 and variant supporting reads of more than 10 was set (--min-coverage 100,  --min-reads2 10)
 pile2snp_cmds=paste0("java -jar /home/logen/programs/VarScan.v2.3.9.jar pileup2snp pile2cns/",base,"_16S.mpileup --min-coverage 100,  --min-reads2 10 >",
@@ -35,10 +39,6 @@ pile2indel_cmds=paste0("java -jar /home/logen/programs/VarScan.v2.3.9.jar pileup
 pile2cns_cmds=paste0("java -jar /home/logen/programs/VarScan.v2.3.9.jar pileup2cns pile2cns/",base,"_16S.mpileup --min-coverage 100,  --min-reads2 10 >",
                      "pile2cns/",base,"_16S.cns")
 cmds=c(bowtie2_cmds,samtools_cmds,pileup_cmds,pile2snp_cmds,pile2indel_cmds,pile2cns_cmds)
-# excute the above commands generated
+# excute commands generated
 for(i in 1:length(cmds)){system(cmds[i])}
-
-
-
-
 
