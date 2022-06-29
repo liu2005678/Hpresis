73585f445ac190225c3ddf1cd91725b048f23a48
liu2005678 <60136942+liu2005678@users.noreply.github.com>
2022-06-29 10:11:34 +0800

    Update 16S.r

--- 16S.r
+++ 16S.r
@@ -7,7 +7,7 @@ strainsr2=strains[grep("hp\\d+.clean.r2.fq.gz",strains)]
 strainsr1;strainsr2
 base=sub(".clean.r1.fq.gz","",strainsr1)
 base
-bowtie2_map=paste0("bowtie2 -p 24 -x index/16s -1 ", cleandatadir, strainsr1, " -2 ", cleandatadir, strainsr2, " -S align/",
+bowtie2_cmds=paste0("bowtie2 -p 24 -x index/16s -1 ", cleandatadir, strainsr1, " -2 ", cleandatadir, strainsr2, " -S align/",
                   base, "_16S.sam" )
 samtools_flow2 =function(files_base,p=24,rm=TRUE){
   cmd6=c();cmd8=c()
@@ -20,6 +20,11 @@ samtools_flow2 =function(files_base,p=24,rm=TRUE){
   return(cmd8)
 }
 samtools_cmds=samtools_flow2(paste0("align/",base,"_16S"))
-
-
+pileup_cmds=paste0("samtools mpileup -f 16S.fa align/",base,"_16S_sorted.bam > pile2cns/",base,"_16S.mpileup") 
+pile2snp_cmds=paste0("java -jar /home/logen/programs/VarScan.v2.3.9.jar pileup2snp pile2cns/",base,"_16S.mpileup --min-coverage 100,  --min-reads2 10 >",
+                     "pile2cns/",base,"_16S.snp")
+pile2indel_cmds=paste0("java -jar /home/logen/programs/VarScan.v2.3.9.jar pileup2indel pile2cns/",base,"_16S.mpileup --min-coverage 100,  --min-reads2 10 >",
+                       "pile2cns/",base,"_16S.indel")
+pile2cns_cmds=paste0("java -jar /home/logen/programs/VarScan.v2.3.9.jar pileup2cns pile2cns/",base,"_16S.mpileup --min-coverage 100,  --min-reads2 10 >",
+                     "pile2cns/",base,"_16S.cns")
 
